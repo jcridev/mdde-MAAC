d02bc55db234df17cc31b32b9436231ccc7d2c1a
akharitonov <32765888+akharitonov@users.noreply.github.com>
2020-05-10 23:48:49 +0200

    Convenience scripts for building docker images no support arguments.

--- docker/compositions/redis/scripts/maac_build.sh
+++ docker/compositions/redis/scripts/maac_build.sh
@@ -1,15 +1,40 @@
 #!/bin/sh
 
 # MDDE-MAAC repo root
-REPO_ROOT=${1:-../../../..}
+REPO_ROOT=../../../..
 # MDDE core location
-MDDE_DIR=${2:-${REPO_ROOT}/mdde}
+MDDE_DIR=${REPO_ROOT}/mdde  # TODO: Copy over into the repo root if outside of it (repo root is the Docker build tontext)
+
+SKIPREGISTRY=0
+for i in "$@"
+do
+case $i in
+    -g=*|--gitreporoot=*)
+    REPO_ROOT="${i#*=}"
+    shift
+    ;;
+    -m=*|--mddecore=*)
+    MDDE_DIR="${i#*=}"
+    shift
+    ;;
+    -r|--regskip) # Skip registry image rebuild
+    SKIPREGISTRY=1
+    shift 
+    ;;
+    *)
+    echo "Unknown argument ${i}"
+    exit 1
+    ;;
+esac
+done
 
 # Build (re-build) default MAAC and all supporting images
 
 # 1. Build the MAAC container based on the sample code
-(cd ../../../images/environment/scripts/ && sh build_maac_local.sh ${REPO_ROOT})
-# 2. Build the MDDE Registry base image
-(cd ${MDDE_DIR}/docker/images/registry/scripts/ && sh build_redis_images.sh)
+(cd ../../../images/environment/scripts/ && sh build_maac_local.sh -g=${REPO_ROOT})
+# 2. Build the MDDE Registry image
+if [ $SKIPREGISTRY -eq 0 ];then
+  (cd ${MDDE_DIR}/docker/images/registry/scripts/ && sh build_redis_images.sh)
+fi
 # 3. Build the final composition, including MDDE Registry image (From MDDE Registry base image) relying on Redis DB for own storgate and data nodes
 docker-compose -f ${MDDE_DIR}/docker/compositions/redis/docker-compose.yml -f ../docker-compose.maac.yml build --no-cache
--- docker/images/environment/scripts/build_maac_local.sh
+++ docker/images/environment/scripts/build_maac_local.sh
@@ -1,9 +1,27 @@
 #bin/sh
 
 # MDDE-MAAC repo root
-REPO_ROOT=${1:-../../../..}
+REPO_ROOT=../../../..
 # MDDE core subfolder
-MDDE_CORE_FOLDER=${2:-./mdde}
+MDDE_CORE_FOLDER=./mdde
+
+for i in "$@"
+do
+case $i in
+    -g=*|--gitreporoot=*)
+    REPO_ROOT="${i#*=}"
+    shift
+    ;;
+    -m=*|--mddecore=*)
+    MDDE_CORE_FOLDER="${i#*=}"
+    shift
+    ;;
+    *)
+    echo "Unknown argument ${i}"
+    exit 1
+    ;;
+esac
+done
 
 # The following command assumes that the MDDE repo (https://github.com/akharitonov/mdde) was checkout in the ./mdde folder of mdde-MAAC repo or copied there.
 # (specifically https://github.com/akharitonov/mdde/mdde subfolder) 
